b48529cfe71bea72aeb6a4abec2fc7330bf6d610
Darkalekei <55298616+Darkalekei@users.noreply.github.com>
2019-09-18 20:48:06 -0500

    Update main.cpp

--- ejercicio7/main.cpp
+++ ejercicio7/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include "Tipos.h"
+#include "string"
 using namespace std;
-
-int main() {
-
-entero numero;
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main () {
+  int numero=0;
+  do{
+  cout<<"ingrese numero: \n";
+  cin>>numero;
+  }while(numero<10 || numero>30);
+  for(int i=0;i<=(numero-1);i++)
+  cout<<"*";
 }
