ad95a0217872fefd315d0d451517386b09db8748
Darkalekei <55298616+Darkalekei@users.noreply.github.com>
2019-09-18 20:19:35 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-#include "Tipos.h"
+#include "string"
+
 using namespace std;
 
-int main() {
+int main(){
+    char letra;
+    int numero;
+    string color;
+    do{
+        cout<<"Ingresa una letra: \n";
+        cin>>letra;
+    }while(letra>'h'|| letra<'a');
+    do{
+        cout<<"Ingresa un numero: \n";
+        cin>>numero;
+    }while(numero>8 || numero<1);
+    if(letra=='a' || letra=='c'||letra=='e'||letra=='g')
+        if(numero%2!=0)
+            color="negro";
+        else
+            color="blanco";
+    else
+        if (numero%2== 0)
+            color = "negro";
+        else
+            color = "blanco";
 
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    cout<<"La posicion "<<letra<<numero<<" tiene un color: "<<color;
+    return 0;
 }
