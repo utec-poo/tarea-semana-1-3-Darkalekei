79fc934085d65ced4ccf4d941abe1038acc2173e
Darkalekei <55298616+Darkalekei@users.noreply.github.com>
2019-09-18 20:11:51 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include "Tipos.h"
+#include <string>
 using namespace std;
-
 int main() {
-
-
-// ToDO
-
+  string zona;
+  double a=212,b=170,c=136,d=59;
+  int precio=0,paga=0;
+  cout<<"ingrese Zona:\n";
+  cin>>zona;
+  cout<<"Precio (1,2,3,4)\n";
+  cin>>precio;
+   if (zona=="claro"){
+    a=a*20/100;
+    b=b*20/100;
+    c=c*20/100;
+    d=d*20/100;
+   }
+  switch(precio){
+    case 1:
+      cout<<"El precio es "<<a<<" soles";
+        break;
+    case 2:
+      cout<<"El precio es "<<b<<" soles";
+        break;
+    case 3:
+      cout<<"El precio es "<<c<<" soles";  
+        break;
+    case 4:
+      cout<<"El precio es "<<d<<" soles";
+        break;
+    default:
+      cout<<"error";
+        break;
+    }
   return 0;
 }
